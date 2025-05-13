5b3af57337e0c86e4ea2bc27853262e583766afe
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-05-13 19:39:48 +0100

    Added cars

---
+++ week5/js/site.js
@@ -0,0 +1,36 @@
+console.log("Hello from site.js");
+
+let car1 = {
+    mf: 'Ford',
+    model: 'Fiesta',
+    engine: '1.2',
+    colour: 'Pink',
+    type: 'Sedan'
+}
+console.log(car1.mf);
+console.log(car1.model);
+
+
+let car2 =    {
+    mf: 'Volkswagen',
+    model: 'Golf',
+    engine: '1.6',
+    colour: 'Red',
+    type: 'Hatchback'
+}
+
+let car3 =   {
+    mf: 'Ford',
+    model: 'Kuga',
+    engine: '1.3',
+    colour: 'Blue',
+    type: 'Hatchback'
+}
+
+
+
+console.log(car2.mf);
+console.log(car2.model);
+
+console.log(car3.mf);
+console.log(car3.model);    
